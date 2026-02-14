d6dd9d7282205b7ea66352711b2be073414e35a9
Vlad Turda <vturda@gmail.com>
2026-02-13 21:39:51 -0500

    Update keyboard mapping to QWERTY layout

--- synth-vue/src/components/Synth.vue
+++ synth-vue/src/components/Synth.vue
@@ -93,10 +93,12 @@ let masterGain = null
 const activeOscillators = new Map()
 
 const keyMap = {
-  'a': 'C3', 'w': 'C#3', 's': 'D3', 'e': 'D#3', 'd': 'E3',
-  'f': 'F3', 't': 'F#3', 'g': 'G3', 'y': 'G#3', 'h': 'A3',
-  'u': 'A#3', 'j': 'B3',
-  'k': 'C4', 'o': 'C#4', 'l': 'D4', 'p': 'D#4', ';': 'E4'
+  'q': 'C3', '2': 'C#3', 'w': 'D3', '3': 'D#3', 'e': 'E3',
+  'r': 'F3', '5': 'F#3', 't': 'G3', '6': 'G#3', 'y': 'A3',
+  '7': 'A#3', 'u': 'B3',
+  'z': 'C4', 's': 'C#4', 'x': 'D4', 'd': 'D#4', 'c': 'E4',
+  'v': 'F4', 'g': 'F#4', 'b': 'G4', 'h': 'G#4', 'n': 'A4',
+  'j': 'A#4', 'm': 'B4'
 }
 
 function getFrequency(noteWithOctave) {
